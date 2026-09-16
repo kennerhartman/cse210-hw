e2f0dbef51500f70ad6dd8caf252f0b64e3a4370
kennerhartman <90115782+kennerhartman@users.noreply.github.com>
2026-09-15 22:13:18 -0600

    finished prep 1 and prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,81 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage is your grade? ");
+
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+
+        char letter;
+
+        if (grade >= 90)
+        {
+            letter = 'A';
+        }
+        else if (grade >= 80)
+        {
+            letter = 'B';
+        }
+        else if (grade >= 70)
+        {
+            letter = 'C';
+        }
+        else if (grade >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        bool isSignedGrade = IsSignedGrade(grade);
+
+        if (isSignedGrade)
+        {
+            char sign = GetSign(grade);
+
+            Console.WriteLine($"You got a grade of {letter}{sign}.");
+        } 
+        else
+        {
+            Console.WriteLine($"You got a grade of {letter}.");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("It looks like you didn't pass the class. Study harder next time!");
+        }
+    }
+
+    static bool IsSignedGrade(int grade)
+    {
+        if (grade >= 94 || grade < 60)
+        {
+            return false;
+        }
+
+        if (grade % 10 <= 3 || grade % 10 >= 7)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    static char GetSign(int grade)
+    {
+        if (grade % 10 <= 3)
+        {
+            return '-';
+        }
+        else
+        {
+            return '+';
+        }
     }
 }
