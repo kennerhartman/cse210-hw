e6da372d494aa09dad11c2b55971d48470175f71
kennerhartman <90115782+kennerhartman@users.noreply.github.com>
2026-09-21 15:45:00 -0600

    completed prep 3 program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int number = random.Next(1, 100);
+
+        int numberOfGuesses = 0;
+
+        while (true)
+        {
+            Console.Write("What is your guess? ");
+
+            string guessString = Console.ReadLine();
+            int guess = int.Parse(guessString);
+
+            if (guess < number) 
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                ++numberOfGuesses;
+
+                Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
+
+                Console.WriteLine("Want to play again? y/n");
+
+                string response = Console.ReadLine();
+
+                if (response == "y")
+                {
+                    numberOfGuesses = 0;
+                    number = random.Next(1, 100);
+                    continue;
+                }
+
+                break;
+            }
+
+            ++numberOfGuesses;
+        }
     }
 }
